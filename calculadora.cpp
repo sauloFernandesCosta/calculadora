dd8928650c505219a1ca0d80b805eaf7c814eb7e
Ragner <ragnerdias@hotmail.com>
2015-06-19 20:34:10 -0300

    Adicionando operação de subtração

--- calculadora.cpp
+++ calculadora.cpp
@@ -6,7 +6,7 @@ main(){
 	
 	printf("-- MINICURSO -- INTRODUCAO AO CONTROLE DE VERSAO C/ GIT -- \n\t -- CALCULADORA SIMPLES EM C -- ");
 	
-	printf("MENU -- \n\n\n 1 - SOMA \n DIGITE O NUMERO DA OPERACAO DESEJADA:  ");
+	printf("MENU -- \n\n\n 1 - SOMA \n 2 - SUBTRACAO \n DIGITE O NUMERO DA OPERACAO DESEJADA:  ");
 	scanf("%d",&op);
 	printf("Digite os numeros desejados: ");
 	scanf("%d",&a);
@@ -16,6 +16,8 @@ main(){
 	system("cls");
 	if(op == 1)
 		op = a+b;
+	else if(op == 2)
+	 	op = a-b;
 		
 	printf("O resultado da operacao e: %d\n\n",op);
 	
